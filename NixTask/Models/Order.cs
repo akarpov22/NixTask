f471529c8026d4b5ea318c962d6db2bd3f117342
Artem Karpov <artem.kar2003@gmail.com>
2021-07-26 04:35:45 +0300

    Step1 successfull

---
+++ NixTask/Models/Order.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using NixTask.Interfaces;
+
+namespace NixTask.Models
+{
+    public class OrderBase : IMain
+    {
+        public static int MaxID = 0;
+        public static List<Order> Orders = new List<Order>();
+        public void Delete(int ID)
+        {
+            foreach (Order order in OrderBase.Orders)
+                if (order.ID == ID)
+                    OrderBase.Orders.Remove(order);
+        }
+    }
+    public class Order
+    {
+        public int ID { get; set; }
+        private DateTime? _creatorDate;
+        public List<(Product product, int Count)> Position { get; set; }
+        public string Comments { get; set; }
+        private Client _client;
+        public DateTime? CreatorDate 
+        {
+            get { return _creatorDate; }
+            set
+            {
+                if (_creatorDate == null)
+                    _creatorDate = value;
+            }
+        }
+        public Client Client
+        {
+            get { return _client; }
+            set
+            {
+                if (_client == null)
+                    _client = value;
+            }
+        }
+        public Order(Client client, List<(Product product, int Count)> position, string comments)
+        {
+            Client = client;
+            Position = position;
+            Comments = comments;
+            CreatorDate = DateTime.Now;
+            ID = OrderBase.MaxID++;
+            OrderBase.Orders.Add(this);
+        }
+    }
+}
---
+++ NixTask/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using NixTask.Interfaces;
+using System.Collections.Generic;
+
+namespace NixTask.Models
+{
+    public class ProductBase : IMain
+    {
+        public static int MaxID = 0;
+        public static List<Product> Products = new List<Product>();
+        public void Delete(int ID)
+        {
+            foreach (Product product in ProductBase.Products)
+                if (product.ID == ID)
+                    ProductBase.Products.Remove(product);
+        }
+    }
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+            ID = ProductBase.MaxID++;
+            ProductBase.Products.Add(this);
+        }
+    }
+}
